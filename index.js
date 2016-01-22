phuedx/pinc

commit b18d30cddf05951417bacc1ec0daa6dac1045522

Surface errors in server code
* Surface error when MAC address resolves to more than one client
* Surface error when setting profile fails
* Surface errors in the API endpoints

Otherwise requests will hang and wait until timeout. Not great for the
consumer of the API.

index.js
@@ -8,6 +8,9 @@ const PORT = process.env.PORT || 8080
 
 const app = express()
 
+const error = (code, res) => (e) =>
+  res.status(code).end(e.message)
+
 app.use(bodyParser.json())
 
 app.get(
@@ -15,6 +18,7 @@ app.get(
   (req, res) =>
     deviceService.getDevices()
       .then((devices) => res.json(devices))
+      .catch(error(500 /* Server Error */, res))
 )
 
 app.get(
@@ -29,6 +33,7 @@ app.get(
 
         res.json(device)
       })
+      .catch(error(500 /* Server Error */, res))
 )
 
 app.put(
@@ -46,9 +51,7 @@ app.put(
       .then(() =>
         res.status(204 /* No Content */).end()
       )
-      .catch(() =>
-        res.status(400 /* Bad Request */).end()
-      )
+      .catch(error(400 /* Bad Request */, res))
 
 )
 

src/dhcp.js
@@ -35,7 +35,7 @@ function getActiveClient (mac) {
     .then((clients) => {
       // If a MAC address resolves to more than one DHCP client, then panic!
       if (clients.length > 1) {
-        throw new Error()
+        throw new Error(`MAC ${mac} resolved to more than one DHCP client`)
       }
 
       return clients.length ? clients[0] : null

src/profiles.js
@@ -31,7 +31,7 @@ function setProfile (ip, profile) {
       `sudo tc filter add dev wlan0 protocol ip parent 1: prio 3 u32 match ip dst ${ip}/32 flowid ${flowID}`,
       (err) => {
         if (err) {
-          return reject()
+          return reject(err)
         }
 
         profiles[ip] = profile

commit 351b15d0b952e6e83ae38783f01a0de5c9ff83da

Fix coding standard errors introduced in b18d30c

index.js
@@ -18,7 +18,7 @@ app.get(
   (req, res) =>
     deviceService.getDevices()
       .then((devices) => res.json(devices))
-      .catch(error(500 /* Server Error */, res))
+      .catch(error(500, res)) // Internal Server Error
 )
 
 app.get(
@@ -27,13 +27,13 @@ app.get(
     deviceService.getDevice(req.params.mac)
       .then((device) => {
         if (!device) {
-          return res.status(404 /* Not Found */)
+          return res.status(404) // Not Found
             .end()
         }
 
         res.json(device)
       })
-      .catch(error(500 /* Server Error */, res))
+      .catch(error(500, res)) // Internal Server Error
 )
 
 app.put(
@@ -42,16 +42,16 @@ app.put(
     deviceService.getDevice(req.params.mac)
       .then((device) => {
         if (!req.body.profile) {
-          return res.status(400 /* Bad Request */)
+          return res.status(400) // Bad Request
             .end()
         }
 
         return deviceService.setDeviceProfile(device, req.body.profile)
       })
       .then(() =>
-        res.status(204 /* No Content */).end()
+        res.status(204).end() // No Content
       )
-      .catch(error(400 /* Bad Request */, res))
+      .catch(error(400, res)) // Bad Request
 
 )
 