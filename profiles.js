e69fc2a809db064246d666a4138165ec43210e02
Sam Smith <me@samsmith.io>
2017-10-20 17:23:41 +0100

    chore(profiles): Add 3G and 4G profiles
    ... based on Ofcom's research into the bandwidth and latency of 4G and
    3G networks in the UK.
    
    This research was discovered as part of #5.

--- profiles.js
+++ profiles.js
@@ -28,5 +28,31 @@ module.exports = [
     name: 'Regular 4G',
     bandwidth: 4000,
     rtt: 20
+  },
+
+  // Based on Ofcom's research into the bandwidth and latency of 4G and 3G
+  // networks in the UK in 2015
+  // (see http://stakeholders.ofcom.org.uk/market-data-research/other/telecoms-research/broadband-speeds/mobile-bb-april-15/).
+  //
+  // Currently, µDL doesn't support configuring download and upload bandwidth.
+  // As it's more likely that the user is downloading via µDL, use the download
+  // bandwidth.
+  {
+    name: '4G (UK)',
+    bandwidth: 14.7,
+    // bandwidth: {
+    //   download: 14.7,
+    //   upload: 13.6
+    //},
+    rtt: 53.1
+  },
+  {
+    name: '3G (UK)',
+    bandwidth: 5.9,
+    // bandwidth: {
+    //   download: 5.9,
+    //   upload: 1.6
+    //},
+    rtt: 63.5
   }
 ]
